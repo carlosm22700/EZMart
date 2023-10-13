be1ff37f0c073b141dd20ace1f036916a9a564fb
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-10-13 01:46:34 -0700

    wip: update TODO

--- market/models.py
+++ market/models.py
@@ -10,3 +10,10 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+'''
+Using Google authentication just for accessing Google Sheets data while using Django's built-in authentication for user login could be a feasible approach.
+
+a user logs in using Django's authentication, but when they want to access their Google Sheets data, they authenticate with Google. Their access and refresh tokens can be stored in the user model, allowing the application to fetch data from Google Sheets on their behalf.
+'''
