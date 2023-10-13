c150a4488a7ac07bb775a8fe1c10699be28606b7
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-10-13 01:54:14 -0700

    wip: possible auth structure

--- market/models.py
+++ market/models.py
@@ -12,8 +12,20 @@ class Item(models.Model):
         return self.name
 
 
-'''
-Using Google authentication just for accessing Google Sheets data while using Django's built-in authentication for user login could be a feasible approach.
+# Using google authentitcation to access google sheets data while using Django's built in authentication for user login could be a feasible approach.
+# A user a user logs in using Django's authentication, but when they want to access their Google Sheets data, they authenticate with Google. Their access and refresh tokens can be stored in the user model, allowing the application to fetch data from Google Sheets on their behalf.
 
-a user logs in using Django's authentication, but when they want to access their Google Sheets data, they authenticate with Google. Their access and refresh tokens can be stored in the user model, allowing the application to fetch data from Google Sheets on their behalf.
-'''
+# Path: market/models.py
+
+# from django.contrib.auth.models import User
+# from django.db import models
+
+
+# class GoogleAuth(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     access_token = models.CharField(max_length=250)
+#     refresh_token = models.CharField(max_length=250)
+#     expires_in = models.PositiveIntegerField()
+#     token_type = models.CharField(max_length=250)
+#     created = models.DateTimeField(auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True)
